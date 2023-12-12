081fb0cd6c933b23f96cb42ba80a2d5f00e0d546
Joshua Oloton <olotonjoshua@gmail.com>
2023-12-12 16:12:04 +0100

    addressing edge case

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,81 +1,44 @@
 #!/usr/bin/python3
 """
-Module: Game of choosing Prime numbers
+Prime Game
 """
 
 
-def is_prime(num):
+def primeNumbers(n):
+    """Return list of prime numbers between 1 and n inclusive
+       Args:
+        n (int): upper boundary of range. lower boundary is always 1
     """
-    Check if a given number is prime.
-
-    Parameters:
-    - num (int): The number to check for primality.
-
-    Returns:
-    - bool: True if the number is prime, False otherwise.
-    """
-    if num < 2:
-        return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
-
-def primeNos(n):
-    """
-    Returns a list of prime numbers between 1 and n inclusive.
-
-    Args:
-        n (int): The upper boundary of the range (inclusive).
-        The lower boundary is always 1.
-
-    Returns:
-        list: A list of prime numbers between 1 and n.
-
-    Examples:
-        >>> primeNos(10)
-        [2, 3, 5, 7]
-
-    Note:
-        The function uses the is_prime helper
-        function to identify prime numbers.
-    """
-    primes = []
-    for i in range(2, n + 1):
-        if is_prime(i):
-            primes.append(i)
-    return primes
+    primeNos = []
+    filtered = [True] * (n + 1)
+    for prime in range(2, n + 1):
+        if (filtered[prime]):
+            primeNos.append(prime)
+            for i in range(prime, n + 1, prime):
+                filtered[i] = False
+    return primeNos
 
 
 def isWinner(x, nums):
     """
-    Determines the winner of the Prime Game for a
-    given number of rounds and upper limits.
-
+    Determines winner of Prime Game
     Args:
-        x (int): The number of rounds of the game.
-        nums (list): A list of upper limits for each round.
-
-    Returns:
-        str or None: The name of the winner (Maria or Ben)
-        or None if the winner cannot be determined.
-
-    Raises:
-        ValueError: If input arguments are invalid
-        (x is None, nums is None, x is not positive, or nums is an empty list).
+        x (int): no. of rounds of game
+        nums (int): upper limit of range for each round
+    Return:
+        Name of winner (Maria or Ben) or None if winner cannot be found
     """
-    if x is None or nums is None or x <= 0 or nums == []:
+    if x is None or nums is None or x == 0 or nums == []:
         return None
-    maria, ben = 0, 0
+    Maria = Ben = 0
     for i in range(x):
-        prime_numbers = primeNos(nums[i])
-        if len(prime_numbers) % 2 == 0:
-            ben += 1
+        primeNos = primeNumbers(nums[i])
+        if len(primeNos) % 2 == 0:
+            Ben += 1
         else:
-            maria += 1
-    if maria < ben:
-        return 'Ben'
-    elif maria > ben:
+            Maria += 1
+    if Maria > Ben:
         return 'Maria'
+    elif Ben > Maria:
+        return 'Ben'
     return None
